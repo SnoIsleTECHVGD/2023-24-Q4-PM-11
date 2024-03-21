b07f65c45044d6cf47e9445327e56e9029741997
MUKILTEO\1717834 <mcklammt@gmail.com>
2024-03-21 11:08:29 -0700

    hdrp settings changes

--- Q4/Assets/Game/Player/Scripts/PlayerMovement.cs
+++ Q4/Assets/Game/Player/Scripts/PlayerMovement.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController characterController;
-
     private float ySpeed;
 
     public float walkSpeed;
@@ -16,7 +15,6 @@ public class PlayerMovement : MonoBehaviour
     {
         characterController = GetComponent<CharacterController>();
     }
-
     void Update()
     {
         Vector3 move = Vector3.zero;
@@ -30,12 +28,10 @@ public class PlayerMovement : MonoBehaviour
             move = (transform.right * horizInput + transform.forward * vertInput).normalized;
         }
 
-
         if (characterController.isGrounded && ySpeed < 0)
         {
             ySpeed = 0f;
         }
-
 
         if(Input.GetKey(KeyCode.LeftShift))
         {
@@ -50,23 +46,19 @@ public class PlayerMovement : MonoBehaviour
         move = new Vector3(move.x, move.y + -vertInput, move.z);
         move = dashAbility(move);
 
-
         characterController.Move(move * Time.deltaTime);
-
     }
-
 
     public float dashSpeed;
     public float dashReturnSpeed;
-
     public float dashTime;
-
     public float dashDistance;
-    private Vector3 dashVector;
 
+    private Vector3 dashVector;
     private Vector3 currentDash;
 
     private float dashInternalTimer = 9999;
+
     Vector3 dashAbility(Vector3 currentMove)
     {
         if(Input.GetKeyDown(KeyCode.Space))
@@ -79,9 +71,6 @@ public class PlayerMovement : MonoBehaviour
                 dashVector = (transform.right * horizInput + transform.forward * vertInput).normalized * dashDistance;
                 dashInternalTimer = 0;
             }
-           
-
-
         }
         else
         {
@@ -90,18 +79,13 @@ public class PlayerMovement : MonoBehaviour
             if (dashInternalTimer > dashTime)
             {
                 currentDash = Vector3.Lerp(currentDash, Vector3.zero, Time.deltaTime * dashReturnSpeed);
-
             }
             else
             {
                 currentDash = Vector3.MoveTowards(currentDash, dashVector, Time.deltaTime * dashSpeed);
             }
-
         }
 
         return currentMove + currentDash;
-
-
-
     }
 }
