0b060047e722307ea594c0c3d5412ef60883cc56
MUKILTEO\1713882 <1713882@mukilteo.wednet.edu>
2024-04-26 12:38:00 -0700

    Fixed Credit scenes.

--- Q4/Assets/Game/UI/MenuUI.cs
+++ Q4/Assets/Game/UI/MenuUI.cs
@@ -9,6 +9,10 @@ public class MenuUI : MonoBehaviour
     public GameObject StartMenu;
     public GameObject CreditMenu;
     public GameObject SettingsMenu;
+    public GameObject DylanPanel;
+    public GameObject MarcusPanel;
+    public GameObject SophiaPanel;
+    public GameObject FinchPanel;
 
     private void Start()
     {
@@ -26,6 +30,45 @@ public class MenuUI : MonoBehaviour
         StartMenu.SetActive(false);
         CreditMenu.SetActive(true);
         
+    }
+
+    public void ClearCredits()
+    {
+        DylanPanel.SetActive(false);
+        MarcusPanel.SetActive(false);
+        SophiaPanel.SetActive(false);
+        FinchPanel.SetActive(false);
+    }
+    public void Dylan()
+    {
+        DylanPanel.SetActive(true);
+        MarcusPanel.SetActive(false);
+        SophiaPanel.SetActive(false);
+        FinchPanel.SetActive(false);
+    }
+
+    public void Marcus()
+    {
+        DylanPanel.SetActive(false);
+        MarcusPanel.SetActive(true);
+        SophiaPanel.SetActive(false);
+        FinchPanel.SetActive(false);
+    }
+
+    public void Sophia()
+    {
+        DylanPanel.SetActive(false);
+        MarcusPanel.SetActive(false);
+        SophiaPanel.SetActive(true);
+        FinchPanel.SetActive(false);
+    }
+
+    public void Finch()
+    {
+        DylanPanel.SetActive(false);
+        MarcusPanel.SetActive(false);
+        SophiaPanel.SetActive(false);
+        FinchPanel.SetActive(true);
     }
 
     public void Exit()
